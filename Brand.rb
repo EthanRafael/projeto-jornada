bd93d0afd65e60bf6edb5bbad70e22fe842fed5a
citi <citi@ug4c15.windows.cin.ufpe.br>
2016-02-16 11:39:13 -0300

    Commit inicial do projeto

---
+++ Brand.rb
@@ -0,0 +1,11 @@
+	Class Brand
+
+	   attr_accessor :brand_name, :type_shoe, :cnpj 
+		
+	   def  initialize (brand_name, type_shoe, cnpj)
+		brand_name = brand_name
+		type_shoe = type_shoe
+		cnpj = cnpj
+	   end
+	end	
+	   
---
+++ Client.rb
@@ -0,0 +1,16 @@
+	Class Client
+	  
+	  attr_accessor :name, :sex, :address, :username, :password, :e_mail, :cpf, :rg
+
+	  def initialize (name, sex, address, username, password, e_mail)
+		name = name
+		sex = sex
+		address = address
+		username = username
+		password = password
+		e_mail = e_mail
+		cpf = cpf
+		rg = rg
+	  end
+	end 	
+	
